8c0d3002976009e39ab718d02e4b0388b22b2569
maikcosta@gmail.com <Mk1311cts>
2021-09-15 13:35:01 -0300

    Rename branch to main

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class PersonController {
     @GetMapping
     public String getBook(){
-        return "Api Teste!";
+        return "Api Teste1!";
     }
 }
